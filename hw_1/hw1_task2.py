45d00beec57f598bff00759a77f6a31d39fb6bb2
shpakovaei <e.i.shpakova@mail.ru>
2023-08-02 22:50:01 +0600

    hw_1

---
+++ hw_1/hw1_task2.py
@@ -0,0 +1,20 @@
+#2.
+
+num = int(input("Введите число: "))
+MAX_NUM = 100000
+MIN_NUM = 0
+k = 0
+
+if num < MIN_NUM or num > MAX_NUM:
+    print("Введено недопустимое число. Допустимы числа от 0 до 100000.")
+elif num == 1 or num == 0:
+    print("Число не является ни простым, ни составным.")
+else:
+    for i in range(2, num // 2 + 1):
+        if (num % i == 0):
+            k = k+1
+    if (k <= 0):
+        print("Число простое")
+    else:
+        print("Число составное")
+
